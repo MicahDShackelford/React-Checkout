c242f9487273e1d122e0ecfeb91f3eb05d37108e
Micah Shackelford <micahdshackelford@gmail.com>
2019-11-20 23:34:36 -0500

    Pushes data up to server, still needs credential verification before pushing to db

--- server/server.js
+++ server/server.js
@@ -9,8 +9,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', express.static('../client/public'));
 
 app.post('/api/checkout', (req,res,next) => {
-  console.log(req.body);
-  res.end("thanks")
+  let user = req.body.user;
+  let shipping = req.body.shipping;
+  let payment = req.body.payment;
+
+  connection.query('INSERT INTO accounts SET firstName=?,lastName=?,email=?,password=?,mailingList=?,registerDate=?', [user.firstName, user.lastName, user.email, user.password, user.mailingList, user.regusterDate]);
+
+  res.statusCode = 200;
+  res.end();
+
   next();
 });
 
